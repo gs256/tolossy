5dbc58cd5698853fc11392ee22057cd8f027e6ad
gs256 <51964952+gs256@users.noreply.github.com>
2026-06-09 23:53:23 +0300

    core: test axum server

--- core/src/main.rs
+++ core/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    Router,
+    response::{Html, IntoResponse},
+    routing::get,
+};
+
+const HOST: &str = "127.0.0.1:2479";
+
+async fn index() -> impl IntoResponse {
+    Html("<h1>TEST</h1>")
+}
+
+fn create_app() -> Router {
+    Router::new().route("/", get(index))
+}
+
+#[tokio::main]
+async fn main() {
+    let app = create_app();
+    let listener = tokio::net::TcpListener::bind(HOST)
+        .await
+        .expect("failed to bind tcp listener");
+
+    println!("Server running http://{HOST}");
+
+    axum::serve(listener, app)
+        .await
+        .expect("failed to start server");
 }
