ec4533ab7f7cc419c591965fa030b370f34535ce
gs256 <51964952+gs256@users.noreply.github.com>
2026-06-11 21:39:15 +0300

    ui: show ffmpeg logs

---
+++ ui/src/error-modal/ErrorModal.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useRef } from "react";
+import type { ProcessingItem } from "../processing-list/useProcessingStore";
+
+export function ErrorModal(props: {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  item: ProcessingItem;
+}) {
+  const { open, setOpen, item } = props;
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  useEffect(() => {
+    if (open && !dialogRef.current.open) {
+      dialogRef.current.show();
+    } else if (!open && dialogRef.current.open) {
+      dialogRef.current.close();
+    }
+  }, [open]);
+
+  function closeErrorModal() {
+    setOpen(false);
+  }
+
+  return (
+    <dialog ref={dialogRef} style={{ maxWidth: "90vw" }}>
+      <div>
+        <h1>{item.name}</h1>
+        <pre>
+          <code style={{ textWrap: "wrap" }}>{item.logs}</code>
+        </pre>
+        <button onClick={closeErrorModal}>Close</button>
+      </div>
+    </dialog>
+  );
+}
--- ui/src/processing-list/ProcessingList.tsx
+++ ui/src/processing-list/ProcessingList.tsx
@@ -1,15 +1,30 @@
-import { useProcessingStore } from "./useProcessingStore";
+import { useState } from "react";
+import { useProcessingStore, type ProcessingItem } from "./useProcessingStore";
+import { ErrorModal } from "../error-modal/ErrorModal";
 
 export function ProcessingList() {
   const { items } = useProcessingStore();
+  const [open, setOpen] = useState(false);
+  const [item, setItem] = useState<ProcessingItem>(null);
+
+  function showLogs(item: ProcessingItem) {
+    setItem(item);
+    setOpen(true);
+  }
 
   return (
-    <div>
-      {items.map((item) => (
-        <div key={item.name}>
-          {item.name} [{item.status}]
-        </div>
-      ))}
-    </div>
+    <>
+      <div>
+        {items.map((item) => (
+          <div key={item.name}>
+            <span>
+              {item.name} [{item.status}]
+            </span>
+            {item.logs && <button onClick={() => showLogs(item)}>logs</button>}
+          </div>
+        ))}
+      </div>
+      {item && <ErrorModal open={open} setOpen={setOpen} item={item} />}
+    </>
   );
 }
